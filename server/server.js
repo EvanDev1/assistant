d216697e5e0cf351625e9f7c9b06cc71f0564cba
Evan Larsen <evanlarsen2008@gmail.com>
2023-01-08 21:33:17 -0700

    Server update again

--- server/server.js
+++ server/server.js
@@ -40,6 +40,12 @@ app.post("/", async (req, res) => {
       bot: response.data.choices[0].text,
     });
 
+    // {
+    //   prompt: 'hey',
+    //   model: 'text-davinci-003',
+    //   maxTokens: '539',
+    //   temp: '0.46'
+    // }
   } catch (error) {
     console.log(error);
     res.status(500).send({ error });
