b6a915c56c1095de2db85ec813365d2b97c7ba61
Evan Larsen <evanlarsen2008@gmail.com>
2023-01-14 19:55:11 -0700

    Please work

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.status(200).send({
-    message: "Server is running properly",
+    message: `${process.env.OPENAI_API_KEY}`,
   });
 });
 
@@ -25,7 +25,7 @@ app.post("/", async (req, res) => {
   try {
     const data = req.body;
     console.log(data);
-    console.log(process.env.OPENAI_API_KEY);
+    
     const response = await openai.createCompletion({
       model: `${data.model}`,
       prompt: `${data.prompt}`,
@@ -37,11 +37,11 @@ app.post("/", async (req, res) => {
     });
 
     console.log(response);
+    console.log(process.env.OPENAI_API_KEY);
 
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).send({ error });
